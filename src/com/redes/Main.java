dbb39a39067c97b304064ff74e98b87d35092c80
Daniel Gonzalez <daa.gonzalez@gmail.com>
2017-10-28 12:42:36 -0600

    Línea de comandos básica

--- src/com/redes/Main.java
+++ src/com/redes/Main.java
@@ -9,21 +9,57 @@ public class Main {
     public static void main(String[] args) {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String input = "";
+        boolean entradaInvalida;
         try {
             while (!input.equalsIgnoreCase("exit")) {
+                entradaInvalida = false;
                 System.out.print("Ingrese un comando> ");
                 input = in.readLine();
-                if(input.equals("start")) {
-                    System.out.println("Inicia el funcionamiento del router.");
+                switch (input.split(" ")[0]) {
+                    case "start":
+                        System.out.println("Iniciar");
+                        break;
+                    case "stop":
+                        System.out.println("Detener (No soportado)");
+                        break;
+                    case "add":
+                        //Revisar que después de add venga una red válida
+                        System.out.println("Agregar una red (No soportado).");
+                        break;
+                    case "show":
+                        if (input.split(" ").length > 1) {
+                            if (input.split(" ")[1].equals("routes")) {
+                                System.out.println("Muestra todas las rutas");
+                            }
+                            else {
+                                entradaInvalida = true;
+                            }
+                        }
+                        else {
+                            entradaInvalida = true;
+                        }
+                        break;
+                    case "help":
+                        System.out.println( "\tstart:\t\tInicia el funcionamiento del enrutador.\n" +
+                                "\tstop:\t\tDetiene el funcionamiento del enrutador. Esto elimina toda la\n" +
+                                "información de rutas y detiene la comunicación con los vecinos.\n" +
+                                "\tadd <subred>:\tAgrega una red directamente conectada al enrutador.\n" +
+                                "Este debe propagar la información a los demás AS’s.\n" +
+                                "\tshow routes:\tMuestra en pantalla todas las rutas aprendidas y agrega *\n" +
+                                "a las que deberían usarse (menor cantidad de AS’s).\n" +
+                                "El formato requerido es el siguiente:\n" +
+                                "red <dir red> : AS<num> - AS<num> - AS<num>\n" +
+                                "\thelp:\t\tMuestra esta ayuda.\n" +
+                                "\texit:\t\tTermina la ejecución de la aplicación.\n");
+                        break;
+                    default:
+                        if (!input.equals("exit")) {
+                            entradaInvalida = true;
+                        }
+                        break;
                 }
-                else if(input.equals("stop")) {
-                    System.out.println("Detiene el funcionamiento del router. (No soportado)");
-                }
-                else if(input.equals("add")) {
-                    System.out.println("Agrega una red directamente conectada al router (No soportado).");
-                }
-                else if(input.equals("show routes")) {
-                    System.out.println("Muestra todas las rutas aprendidas y agrega un * a las que deban usarse.");
+                if(entradaInvalida) {
+                    System.out.println("Comando inválido. Escriba 'help' para ver una lista de los comandos aceptados.\n");
                 }
             }
         } catch (IOException e) {
